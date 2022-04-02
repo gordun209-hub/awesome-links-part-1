f21e6f2ef62e905f35697604a73f23c42e7f0342
gordun209 <ali-han80@hotmail.com>
2022-04-02 23:23:13 +0300

    pagination ab0

--- graphql/types/Link.ts
+++ graphql/types/Link.ts
@@ -1,28 +1,19 @@
+// /graphql/types/Link.ts
+
 import { extendType, intArg, objectType, stringArg } from 'nexus'
 
-import { User } from './User'
+// get ALl Links
 
 export const Link = objectType({
   name: 'Link',
   definition(t) {
     t.string('id')
+    t.int('index')
     t.string('title')
     t.string('url')
     t.string('description')
     t.string('imageUrl')
     t.string('category')
-    t.list.field('users', {
-      type: User,
-      async resolve(parent, _args, ctx) {
-        return await ctx.prisma.link
-          .findUnique({
-            where: {
-              id: parent.id,
-            },
-          })
-          .users()
-      },
-    })
   },
 })
 
@@ -37,39 +28,61 @@ export const LinksQuery = extendType({
       },
       async resolve(_, args, ctx) {
         let queryResults = null
+
         if (args.after) {
+          // check if there is a cursor as the argument
           queryResults = await ctx.prisma.link.findMany({
-            take: args.first,
-            skip: 1,
+            take: args.first, // the number of items to return from the database
+            skip: 1, // skip the cursor
             cursor: {
-              id: args.after,
+              id: args.after, // the cursor
             },
           })
         } else {
+          // if no cursor, this means that this is the first request
+          //  and we will return the first items in the database
           queryResults = await ctx.prisma.link.findMany({
             take: args.first,
           })
         }
+        // if the initial request returns links
         if (queryResults.length > 0) {
-          const lastLinkResults = queryResults[queryResults.length - 1]
-          const myCursor = lastLinkResults.id
+          // get last element in previous result set
+          const lastLinkInResults = queryResults[queryResults.length - 1]
+          // cursor we'll return in subsequent requests
+          const myCursor = lastLinkInResults.id
+
+          // query after the cursor to check if we have nextPage
           const secondQueryResults = await ctx.prisma.link.findMany({
             take: args.first,
             cursor: {
               id: myCursor,
             },
+            orderBy: {
+              createdAt: 'asc',
+            },
           })
+          // return response
           const result = {
             pageInfo: {
               endCursor: myCursor,
-              hasNextPage: secondQueryResults.length >= args.first,
+              hasNextPage: secondQueryResults.length >= args.first, //if the number of items requested is greater than the response of the second query, we have another page
             },
             edges: queryResults.map(link => ({
               cursor: link.id,
               node: link,
             })),
           }
+
           return result
+        }
+        //
+        return {
+          pageInfo: {
+            endCursor: null,
+            hasNextPage: false,
+          },
+          edges: [],
         }
       },
     })
@@ -81,7 +94,7 @@ export const Edge = objectType({
   definition(t) {
     t.string('cursor')
     t.field('node', {
-      type: Link,
+      type: 'Link',
     })
   },
 })
